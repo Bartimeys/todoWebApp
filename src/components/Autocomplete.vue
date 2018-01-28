c11c2931f880a0fbcac77a775c74de187e7cc221
Olha Popovych <bartimeusurucskiy@gmail.com>
2018-01-28 22:52:27 +0200

    fix db bug

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input class="new-todo" placeholder="What needs to be done?" autofocus>
+  </header>
+</template>
+<script lang="ts">
+  export default {
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
